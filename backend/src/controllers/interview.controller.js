fedd835c4ef1a6d8b7eddf0edff1b7e7130518b8
thambahadurpun <thambahadurpun123@gmail.com>
2026-03-02 15:44:29 +0545

    feat: implement interview report generation with PDF parsing and user association

--- backend/src/controllers/interview.controller.js
+++ backend/src/controllers/interview.controller.js
@@ -1,5 +1,33 @@
-async function generateInterviewReportController(req, res) {
+const pdfParse = require('pdf-parse')
+const generateInterviewReport = require('../services/ai.service')
+const interviewReportModel = require('../models/interviewReport.model')
 
+async function generateInterviewReportController(req, res) {
+    const resumeFile = req.file
+    const { selfDescription, jobDescription } = req.body
+
+    const resumeContent = await (new pdfParse.PDFParse(resumeFile.buffer)).getText()
+
+    try {
+        const report = await generateInterviewReport({
+            resume: resumeContent.text,
+            selfDescription,
+            jobDescription
+        })
+
+        const interviewReport = await interviewReportModel.create({
+            user: req.user._id,
+            resume: resumeContent,
+            selfDescription,
+            jobDescription,
+            ...report,
+        })
+
+        res.status(201).json({ message: "Interview report generated successfully", data: interviewReport })
+    } catch (error) {
+        console.error("Error generating interview report:", error)
+        res.status(500).json({ error: "Failed to generate interview report" })
+    }
 }
 
 module.exports = {
--- backend/src/routes/interview.routes.js
+++ backend/src/routes/interview.routes.js
@@ -6,11 +6,13 @@ const authMiddleware = require('../middlewares/auth.middleware');
 
 const interViewController = require('../controllers/interview.controller');
 
+const upload = require('../middlewares/file.middleware')
+
 /**
  * @route POST api/interview/generate-report
  * @desc Generate an interview report based on the user's resume, self-description, and job description
  * @access Private
  */
-interviewRouter.post('/generate-report', authMiddleware.authUser, interViewController.generateInterviewReportController)
+interviewRouter.post('/generate-report', authMiddleware.authUser, upload.single("resume"), interViewController.generateInterviewReportController)
 
 module.exports = interviewRouter
